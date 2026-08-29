244dab3d31c4d8fe1fb2dfed6ab1cc680f8c1c47
zjlin-42 <s1213096326@icloud.com>
2026-08-28 18:27:49 -0600

    Add files via upload

--- demo.m
+++ demo.m
@@ -1,6 +1,6 @@
 %% Demo for directional contact angle hysteresis
 
-epsilon = 1e-2;
+tau = 1e-2;
 N = 64;
 p = 6;
 q = 6;
@@ -11,13 +11,13 @@ p = p/gcd_pq;
 q = q/gcd_pq;
 
 % Receding case
-zoomin3D(epsilon, N, "right", p, q);
+zoomin3D(tau, N, "right", p, q);
 filename_rec = sprintf('3D_right_%d_%d_1e-3_zoomin.mat', p, q);
 data_rec = load(filename_rec);
 theta_rec = data_rec.final_contact_angle;
 
 % Advancing case
-zoomin3D(epsilon, N, "left", p, q);
+zoomin3D(tau, N, "left", p, q);
 filename_adv = sprintf('3D_left_%d_%d_1e-3_zoomin.mat', p, q);
 data_adv = load(filename_adv);
 theta_adv = data_adv.final_contact_angle;
